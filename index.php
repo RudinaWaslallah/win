5887d7dcf6e77309e2bcb474ed3f65e5efd96301
RudinaWaslallah <rudinawasx@gmail.com>
2023-11-10 23:28:22 +0300

    init

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
+$conn =mysqli_connect('localhost','root','root','win');
+
+if($conn){
+    echo 'success';
+}else{
+    echo'fail';
+}
+
+
+
 $firstName=    $_POST['firstName'];
 $lastName=     $_POST['lastName'];
 $email=        $_POST['email'];
 
-if(isset($_POST['submit'])){
-echo $firstName .' '. $lastName .' '. $email ;}
+if(isset($_POST['Submit'])) {
+echo $firstName .'  '. $lastName .'  '. $email;
+}
 ?>
 
 <!DOCTYPE html>
@@ -16,9 +27,9 @@ echo $firstName .' '. $lastName .' '. $email ;}
     <title>Document</title>
 </head>
 <body>
-<form action="index.php" method="post" >
-    <input type="text" name="firstname" id="firstname" placeholder="First Name">
-    <input type="text" name="lastname" id="lastname" placeholder="Last Name">
+<form action="index.php" method="POST">
+    <input type="text" name="firstName" id="firstName" placeholder="First Name">
+    <input type="text" name="lastName" id="lastName" placeholder="Last Name">
     <input type="text" name="email" id="email" placeholder="Email">
     <input type="Submit" name="Submit"  value="Send">
 </form>
